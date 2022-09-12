45f4dd8ce3afc658a4a14020af9878d695b75a92
sinem sipahi <sipahisnm@gmail.com>
2022-09-12 17:07:18 -0400

    first py file

---
+++ flask-01-hello-word-app/hello-world-app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+
+
+app = Flask (__name__)
+
+@app.route("/")
+def head():
+        return "hello world"
+
+@app.route('/sinem')
+def second():
+    return "This is the Sinem's page"
+
+
+@app.route('/third/subthird')
+def third():
+    return "This is the subpage of third page"
+
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f'Id of this page is {id}'
+
+
+
+
+if __name__ == '__main__': 
+        app.run(debug = True)

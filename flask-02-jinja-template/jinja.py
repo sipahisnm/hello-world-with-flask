9aaba86fd0c1adc74de4ae155cf3d19d7687fc76
sinem sipahi <sipahisnm@gmail.com>
2022-09-12 17:44:22 -0400

    jinja

--- flask-02-jinja-template/jinja.py
+++ flask-02-jinja-template/jinja.py
@@ -1,4 +1,5 @@
 
+from tkinter import Variable
 from flask import Flask ,render_template
 
 app = Flask(__name__)
@@ -6,6 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 def  head():
         return render_template("index.html",number=12)
+
+
+
+@app.route("/sinem")
+def number():
+        variable1 , variable2 = 23 ,45
+        return render_template('body.html',num1=variable1, num2=variable2)
 
 
 if __name__=="__main__":
